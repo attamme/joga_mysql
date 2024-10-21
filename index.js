81c149e9c4bd01960ab5955d1d7e6c27e26fb0df
Annaliisa Tamme <annaliisa.tamme@voco.ee>
2024-10-21 08:42:37 +0300

    Created project basic files

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-testing
+const express = require('express');
+const path = require('path');
+const mysql = require('mysql2');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'qwerty',
+    database: 'joga_mysql'
+});
+
+con.connect((err) => {
+    if (err) throw err; 
+    console.log('Connected to the database');
+})
+
+app.listen(3003, () => {
+    console.log('App is started at http://localhost:3003');
+})
