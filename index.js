4aa8e17e5b4bfa563538a937e572920076af0513
Annaliisa Tamme <annaliisa.tamme@voco.ee>
2024-10-21 09:41:55 +0300

    Displayed all articles

--- index.js
+++ index.js
@@ -32,6 +32,16 @@ con.connect((err) => {
     console.log('Connected to the database');
 })
 
+app.get('/', (req, res) => {
+    let query = "SELECT * FROM article";
+    let articles = [];
+    con.query(query, (err, result) => {
+        if (err) throw err;
+        articles = result
+        res.render('index', { articles: articles });
+    })
+});
+
 app.listen(3003, () => {
     console.log('App is started at http://localhost:3003');
 })
